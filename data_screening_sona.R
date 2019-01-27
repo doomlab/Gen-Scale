01c83a2c9878ad85739852314cd6b9e5e42859c5
Ari Cunningham <arielle924@gmail.com>
2019-01-27 16:16:43 -0600

    accuracy and missing done
    Can't get SADfunction script to import

--- data_screening_sona.R
+++ data_screening_sona.R
@@ -2,6 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 master = read.csv("sona_data.csv")
+colnames(master)[1] = "StartDate"
 
 block1 = c(24:48)
 block2 = c(49:72)
@@ -9,27 +10,58 @@ block3 = c(73:96)
 
 scale_questions = c(24:44,49:68,73:92)
 
-### Accuracy ###
+
+# Accuracy ----------------------------------------------------------------
 
 notypos = master
-summary(notypos[-c(1:2), scale_questions])
-notypos[,24]
-levels(notypos[,24])
+summary(notypos[scale_questions])
+#everything is within the expected range
+
+apply(notypos[scale_questions], 2, mean, na.rm = TRUE)
+apply(notypos[scale_questions], 2, sd, na.rm = TRUE)
+#nothing seems terribly wacky
 
 
-### Missing ###
+# Missing -----------------------------------------------------------------
+
+percentmiss = function(x){ sum(is.na(x))/length(x) *100 }
+
+#rows
+missing = apply(notypos[scale_questions], 1, percentmiss) 
+table(missing)
+replacepeople = subset(notypos, missing <= 5)
+dontpeople = subset(notypos, missing > 5)
+
+#columns
+apply(replacepeople[scale_questions], 2, percentmiss)
+table(missingcol[scale_questions])
+replacecolumn = replacepeople[scale_questions]
+dontcolumn = replacepeople[ , -c(24:44,49:68,73:92)]
+
+#replacing those with <= 5% missing by multiple imputation
+library(mice)
+tempnomiss = mice(replacecolumn)
+nomiss = complete(tempnomiss, 1)
+summary(nomiss)
+
+#putting data back together
+#excluding participants with > 5% missing
+allcolumns = cbind(dontcolumn, nomiss)
+summary(allcolumns)
+nomissing = allcolumns
+
+
+# SAD ---------------------------------------------------------------------
+
+source("SADfunction.R")
+
+
+# Outliers ----------------------------------------------------------------
 
 
 
-### SAD ###
 
-
-
-### Outliers ###
-
-
-
-### Assumptions ###
+# Assumptions -------------------------------------------------------------
 
 # Additivity #
 
@@ -41,3 +73,4 @@ levels(notypos[,24])
 
 
 # Homog/s #
+
